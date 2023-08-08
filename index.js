32fac7867d658243603fe8ead50d0a4ac7fd027c
Mark Strempel <mark.strempel@codecool.com>
2023-08-08 15:58:55 +0200

    Implement histogram calculation.

--- index.js
+++ index.js
@@ -16,3 +16,52 @@ inputFileSelect.addEventListener("change", function (event) {
     })
     fileReader.readAsDataURL(file);
 });
+
+const improveImageButton = document.getElementById("improve-image-button");
+improveImageButton.addEventListener("click", function() {
+    const imageData = canvasContext.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+    const histogram = analyzePixels(imageData.data, imageCanvas.width, imageCanvas.height);
+    const minMaxByColor = {
+        red: [findMinimumColorValue(histogram.red), findMaximumColorValue(histogram.red)],
+        green: [findMinimumColorValue(histogram.green), findMaximumColorValue(histogram.green)],
+        blue: [findMinimumColorValue(histogram.blue), findMaximumColorValue(histogram.blue)],
+    }
+});
+
+function analyzePixels(pixels, width, height) {
+    const colorArrays = [(new Array(255)).fill(0), (new Array(255)).fill(0), (new Array(255)).fill(0)];
+
+    for (let widthIndex = 0; widthIndex < width; widthIndex++) {
+        for (let heightIndex = 0; heightIndex < height; heightIndex++) {
+            for (let colorIndex = 0; colorIndex < 4; colorIndex++) {
+                const index = colorIndex + widthIndex * 4 + heightIndex * width * 4;
+
+                let colorArray = colorArrays[colorIndex];
+
+                if (colorArray !== undefined) {
+                    const colorValue = pixels[index];
+                    colorArray[colorValue] += 1;
+                }
+            }
+        }
+    }
+
+    return {
+        red: colorArrays[0],
+        green: colorArrays[1],
+        blue: colorArrays[2],
+    };
+}
+
+function findMinimumColorValue(colorArray) {
+    return colorArray.findIndex(colorValue => colorValue > 0);
+}
+
+function findMaximumColorValue(colorArray) {
+    return colorArray.findLastIndex(colorValue => colorValue > 0);
+}
+
+function mapValue(originMin, originMax, value, targetMin, targetMax) {
+    const intervalLengthStretchfactor = (targetMax - targetMin) / (originMax - originMin)
+    return targetMin + (value - originMin) * intervalLengthStretchfactor;
+}
